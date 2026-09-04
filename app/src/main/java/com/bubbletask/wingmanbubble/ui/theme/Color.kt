24c8406426df2532bb26e1dbf2cf4f86be02fc45
Ant491971 <125981180+Ant491971@users.noreply.github.com>
2026-09-04 13:33:21 +0100

    upload app/src/main/java/com/bubbletask/wingmanbubble/ui/theme/Color.kt

---
+++ app/src/main/java/com/bubbletask/wingmanbubble/ui/theme/Color.kt
@@ -0,0 +1,8 @@
+package com.bubbletask.wingmanbubble.ui.theme
+import androidx.compose.ui.graphics.Color
+val Blue80 = Color(0xFFADC6FF)
+val BlueGrey80 = Color(0xFFC3C6CF)
+val Sky80 = Color(0xFFD1E4FF)
+val Blue40 = Color(0xFF325DA8)
+val BlueGrey40 = Color(0xFF5A5E66)
+val Sky40 = Color(0xFF4C6078)
